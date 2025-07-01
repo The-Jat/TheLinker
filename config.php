19a20071e300d68c879cdad1daabbad2f333dde4
MaK <manishkl543@gmail.com>
2025-07-01 10:40:14 +0530

    Stop tracking config.php and ignore it

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     'bio_links');
-define('SITE_URL',          'http://localhost/66biolink-4400nulled/');
